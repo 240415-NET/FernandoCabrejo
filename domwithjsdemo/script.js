166ba3a1b5ac139ac5878e63961b1569c44e7c66
Fernando Cabrejo <fcabrejo@geico.com>
2024-06-11 10:28:06 -0400

    adding js

---
+++ domwithjsdemo/script.js
@@ -0,0 +1,15 @@
+//this will be our javascript
+//It is attached to our HTML file via a script tag at the bottom of the body element.
+
+//So, first we have to wait for the DOM to fully finish loading.
+//Inside of our javascript file, we can reference all the HTMOL (and the DOM structure) of 
+//our index.html, using the "document" object.  We don't have to instantiate it or create it or anything,
+//we can just start using it
+
+//We referemce pit dpci,emt pbkect. then use dot notation to run a function to add an event listener.
+//The event we are going to listen for, is 'DomContentLoaded'.
+document.addEventListener('DOMContentLoaded', (event) => {
+
+
+
+}); //Closing the addEventListener method
