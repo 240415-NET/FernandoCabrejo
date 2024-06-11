661f35139dd4effd4d0c23a62956753748a887c9
Fernando Cabrejo <fcabrejo@geico.com>
2024-06-11 10:39:17 -0400

    simple lambdas

--- domwithjsdemo/script.js
+++ domwithjsdemo/script.js
@@ -8,8 +8,24 @@
 
 //We referemce pit dpci,emt pbkect. then use dot notation to run a function to add an event listener.
 //The event we are going to listen for, is 'DomContentLoaded'.
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+//All of our DOM manipulation will be done within this lambda within addEventListener
 
+//So now, we will start to work with our HTML element as Javascript variables/objects
+//To select our button (in our HTML) we will use getelementById
+const changeTextButton = document.getElementById('changeTextButton');
 
+//Now we will add a listener to that button we selected and store in changeTextbutton above
+changeTextButton.addEventListener('click', () => {
 
-}); //Closing the addEventListener method
+    console.log('Yep, my button was indeed clicked!');
+
+    //To update the text inside of out p-tag, we will select it via it's ID, as we did for the button above
+    //the name of our Javascript variable does not need to match the name of the ID in the html element w34e select
+    const paragraph = document.getElementById('textToChanges');
+    
+    paragraph.textContent = 'This is my new text that is coming in from my Javascript code!';
+    
+
+    });//Closing the button.addEventListener() method
+}); //Closing the addEventListener() method
